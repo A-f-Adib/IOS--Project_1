cdc4ab247808f63ee59705831543550df4f990d3
A-f-Adib <109586107+A-f-Adib@users.noreply.github.com>
2023-08-15 18:25:26 +0600

    Initial Commit

---
+++ Wesplit/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Wesplit
+//
+//  Created by A.f. Adib on 8/15/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
