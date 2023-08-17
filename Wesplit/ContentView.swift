e18346656ff67b96e9e1f9f8d075d3375a5228f2
A-f-Adib <109586107+A-f-Adib@users.noreply.github.com>
2023-08-17 15:02:40 +0600

    f

--- Wesplit/ContentView.swift
+++ Wesplit/ContentView.swift
@@ -8,14 +8,93 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        Text("Hello, world!")
-            .padding()
+    
+    
+    @State private var checkAmount = 0.00
+    @State private var nOfpeople = 0
+    @State private var tipsPer = 10
+    
+    let tipPer = [5, 10, 15, 20,25]
+    
+    var totalPerperson : Double {
+        
+        let peopleCount = Double (nOfpeople + 2)
+        let tipsPercent = Double (tipsPer)
+        
+        let tipValue = checkAmount / 100 * tipsPercent
+        let grandTotal = checkAmount + tipValue
+         let amountPerhead = grandTotal / peopleCount
+        
+        return amountPerhead
     }
+    
+    var body: some View {
+        
+        NavigationView {
+            
+        
+        Form{
+            Section{
+                TextField( "Amount", value: $checkAmount, format: .currency(code: Locale.current.currencyCode ?? "USD"))
+                    .keyboardType(.decimalPad)
+                
+                Picker("Select no of people", selection: $nOfpeople){
+                    ForEach(2..<100){
+                    Text("\($0) people")
+               }
+            }
+                
+        } header : {
+            Text("Total Bill")
+        }
+            Section {
+                Picker("Tip percentage", selection: $tipsPer){
+                    ForEach(tipPer, id: \.self) {
+                        Text($0, format: .percent)
+                    }
+                }
+            }
+            
+           
+            
+            Section{
+                
+                Text(totalPerperson, format: .currency(code: Locale.current.currencyCode ?? "USD"))
+                    
+                 } header : {
+                     Text("Total per person")
+                 }
+            
+               }
+            
+        .navigationTitle("WeSplit")
+           
+        }
+        
+    }
+    
 }
+        
+        
+//        NavigationView{
+//            Form{
+//
+//                Picker("Select any number", selection: $selectedStd){
+//                    ForEach(students, id: \.self){
+//                        Text($0)
+//                    }
+//                }
+//
+//
+//            }
+//        }
+       
+ //       }
+        
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
+        
         ContentView()
     }
 }
